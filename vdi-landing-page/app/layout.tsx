e275bc218284266fc7627dc1a99cf5fb3c96d72d
Zuhair Shad <123764183+Zuhairshad@users.noreply.github.com>
2026-09-02 16:32:56 +0500

    analytics: add Google Tag Manager (GTM-NHZRJ8JZ)

--- vdi-landing-page/app/layout.tsx
+++ vdi-landing-page/app/layout.tsx
@@ -41,7 +41,22 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.variable} data-scroll-behavior="smooth">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NHZRJ8JZ');`,
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NHZRJ8JZ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <a className="skip-link" href="#main-content">Skip to content</a>
         <Nav />
         {children}
